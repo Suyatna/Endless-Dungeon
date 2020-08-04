1fb12b8c2d5cd305dfcc89aa8155b4999c0a39fc
Suyatna <zuyatna@gmail.com>
2020-08-04 17:10:47 +0700

    adding delay when load scene and exit in paused panel

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -179,14 +179,24 @@ public class MenuManager : MonoBehaviour
     IEnumerator LoadingAsynchronously(int sceneIndex)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = false;
 
+        float waitTime = 0;
+        
         loadingPanel.SetActive(true);
         
         while (!operation.isDone)
         {
-            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            waitTime += 0.0015f;
 
-            loadingSlider.value = progress;
+            if (waitTime >= 1)
+            {
+                operation.allowSceneActivation = true;
+            }
+            else
+            {
+                loadingSlider.value = waitTime;   
+            }
 
             yield return null;
         }
